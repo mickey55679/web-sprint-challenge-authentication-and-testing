404737a8cac2193a0cf57971fbb75f00b4c8f4a9
Michaiah Bos <michaiahbos@yahoo.com>
2024-04-16 14:09:51 -0500

    wip test two

--- api/server.test.js
+++ api/server.test.js
@@ -1,12 +1,12 @@
 const request = require("supertest");
 const server = require("./server");
-const db = require('../data/dbConfig')
+const db = require("../data/dbConfig");
 
 test("sanity", () => {
   expect(true).toBe(true);
 });
 beforeEach(async () => {
-  await db("users").truncate(); 
+  await db("users").truncate();
 });
 jest.setTimeout(10000);
 
@@ -14,13 +14,29 @@ describe("[POST] /api/auth/register", () => {
   it("should return 201 status on successful registration", async () => {
     const newUser = { username: "Captain", password: "foobar" };
 
-    const res = await request(server)
-    .post('/api/auth/register')
-    .send(newUser);
+    const res = await request(server).post("/api/auth/register").send(newUser);
 
     expect(res.status).toBe(201);
     expect(res.body).toHaveProperty("username", newUser.username);
     expect(res.body).toHaveProperty("password");
     expect(res.body).toHaveProperty("id");
+  });
+  it('should return 400 status and "username and password required" message when missing username or password', async () => {
+    const noUsername = { password: "foobar" };
+    const noPassword = { username: "Captain Marvel" };
+
+    const res1 = await request(server)
+      .post("/api/auth/register")
+      .send(noUsername);
+
+    const res2 = await request(server)
+      .post("/api/auth/register")
+      .send(noPassword);
+
+    expect(res1.status).toBe(400);
+    expect(res1.body).toHaveProperty("message", "username and password are required");
+
+    expect(res2.status).toBe(400);
+    expect(res2.body).toHaveProperty('message', "username and password are required");
   });
 });
