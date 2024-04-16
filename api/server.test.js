c1c6f4459c7d5f469c4b218b149ed58417740fee
Michaiah Bos <michaiahbos@yahoo.com>
2024-04-16 14:00:59 -0500

    wip test

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,26 @@
-// Write your tests here
+const request = require("supertest");
+const server = require("./server");
+const db = require('../data/dbConfig')
+
 test("sanity", () => {
   expect(true).toBe(true);
+});
+beforeEach(async () => {
+  await db("users").truncate(); 
+});
+jest.setTimeout(10000);
+
+describe("[POST] /api/auth/register", () => {
+  it("should return 201 status on successful registration", async () => {
+    const newUser = { username: "Captain", password: "foobar" };
+
+    const res = await request(server)
+    .post('/api/auth/register')
+    .send(newUser);
+
+    expect(res.status).toBe(201);
+    expect(res.body).toHaveProperty("username", newUser.username);
+    expect(res.body).toHaveProperty("password");
+    expect(res.body).toHaveProperty("id");
+  });
 });
